da348a45ea53a6e89ed57652476e2faab5a63e3d
Arshbir.Singh <sandhu6698@gmail.com>
2020-01-17 18:12:18 +0530

    1st

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
-
+import 'package:geolocator/geolocator.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
